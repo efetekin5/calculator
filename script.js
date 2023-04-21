97328c7cf8f15713ba3926147d2bf4103d4d84a8
efe <121581223+efetekin5@users.noreply.github.com>
2023-04-21 13:39:50 +0300

    made the operator function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const firstNumber = 6;
+const operator = '*';
+const secondNumber = 2;
+
 function add(a, b) {
     return a + b;
 }
@@ -14,7 +18,20 @@ function divide(a, b) {
     return a / b;
 }
 
+function operater(number1, operator, number2) {
+    if (operator == '*') {
+        return multiply(number1, number2);
+    } else if (operator == '+') {
+        return add(number1, number2);
+    } else if (operator == '-') {
+        return substract(number1, number2);
+    } else if (operator == '/') {
+        return divide(number1, number2);
+    }
+}
+
 console.log(add(5,3));
 console.log(substract(5,3));
 console.log(multiply(5,3));
 console.log(divide(6,3));
+console.log(operater(20, '*', 5));
