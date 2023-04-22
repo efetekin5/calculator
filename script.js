197f2458cd00ebfe5793b28ae30473a5c42cb80f
efe <121581223+efetekin5@users.noreply.github.com>
2023-04-22 15:09:46 +0300

    made all the event listeners for all math operations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const firstNumber = 6;
-const operator = '*';
-const secondNumber = 2;
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
 
 function add(a, b) {
     return a + b;
@@ -30,6 +30,8 @@ function operate(number1, operator, number2) {
     }
 }
 
+
+
 function display(element) {
     let displayDiv = document.querySelector('.display');
     displayDiv.innerHTML += `${element.innerHTML}`;
@@ -46,11 +48,49 @@ function delet() {
     displayDiv.innerHTML = newDiv;
 }
 
+
+
 let addition = document.querySelector('.add');
 addition.addEventListener('click', () => {
     let displayDiv = document.querySelector('.display');
-    let operator = displayDiv.innerHTML.slice()
-    let additionNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1);
-    console.log(additionNumber);
+    operator = displayDiv.innerHTML.slice(displayDiv.innerHTML.length-1, displayDiv.innerHTML.length)
+    firstNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1).replace(/\s/g, '');
+    firstNumber = parseInt(firstNumber, 10);
     reset();
+})
+
+let substraction = document.querySelector('.substract');
+substraction.addEventListener('click', () => {
+    let displayDiv = document.querySelector('.display');
+    operator = displayDiv.innerHTML.slice(displayDiv.innerHTML.length-1, displayDiv.innerHTML.length)
+    firstNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1).replace(/\s/g, '');
+    firstNumber = parseInt(firstNumber, 10);
+    reset();
+})
+
+let multiplication = document.querySelector('.multiply');
+multiplication.addEventListener('click', () => {
+    let displayDiv = document.querySelector('.display');
+    operator = displayDiv.innerHTML.slice(displayDiv.innerHTML.length-1, displayDiv.innerHTML.length)
+    firstNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1).replace(/\s/g, '');
+    firstNumber = parseInt(firstNumber, 10);
+    reset();
+})
+
+let division = document.querySelector('.divide');
+division.addEventListener('click', () => {
+    let displayDiv = document.querySelector('.display');
+    operator = displayDiv.innerHTML.slice(displayDiv.innerHTML.length-1, displayDiv.innerHTML.length)
+    firstNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1).replace(/\s/g, '');
+    firstNumber = parseInt(firstNumber, 10);
+    reset();
+})
+
+let equals = document.querySelector('.calculate');
+equals.addEventListener('click', () => {
+    let displayDiv = document.querySelector('.display');
+    secondNumber = displayDiv.innerHTML.slice(0, displayDiv.innerHTML.length-1);
+    secondNumber = parseInt(secondNumber, 10);
+    let result = operate(firstNumber, operator, secondNumber);
+    displayDiv.innerHTML = result;
 })
